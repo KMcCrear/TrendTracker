db39b3e57a990c30c6c5c36f247b51f87abda1bf
KMcCrear <kmccrear1@gmail.com>
2022-02-18 12:01:23 +0000

    CandleStick graph added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import NavBar from "./components/Navbar.jsx";
+import CandleStick from "./components/CandleStick.jsx";
 
 function App() {
 	return (
 		<div className="App">
-			<nav>
-				<NavBar />
-			</nav>
+			<nav>{/* <NavBar /> */}</nav>
+			<CandleStick />
 		</div>
 	);
 }
