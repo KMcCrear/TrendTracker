1c203e634c41b3d6071fd321e93535a4e3c0fd8f
TaylorJCassidy <taylorjcassidy1@gmail.com>
2022-03-05 15:02:55 +0000

    page now loads data and refreshes data on button press

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,19 +1,12 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { CandleStick } from "../components/CandleStick";
-import { returnDataToChart } from "../helpers/updateOnSearch";
 
 const Dashboard = () => {
-	const [chart, setChart] = useState(<CandleStick series={[]} />);
-	const getData = () => {
-		const data = returnDataToChart();
-		console.log(data);
-		setChart(<CandleStick series={data} />);
-	};
+	const chart = <CandleStick ticker="AAPL"/>
 	return (
 		<div className="dashboardContainer">
 			<h1>Dashboard</h1>
 			{chart}
-			<button onClick={() => getData()}>Update data</button>
 		</div>
 	);
 };
