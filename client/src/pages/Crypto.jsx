2de23d211fc6786e42c9dd3b7cf49bebe2ad7ae5
KMcCrear <64017644+KMcCrear@users.noreply.github.com>
2022-03-06 14:08:06 +0000

    Mcapi (#10)
    * MCAPI implimentation
    
    * Rendering Data to page

--- client/src/pages/Crypto.jsx
+++ client/src/pages/Crypto.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import getCryptoData from "../helpers/getCryptoData";
 
-const getData = async () => {
-	const quoteArray = [];
-	const cryptoData = await getCryptoData();
-
-	console.log(cryptoData);
-	// cryptoData.forEach((entry) => {
-	// 	quoteArray.push(entry.quote);
-	// });
-	// console.log(quoteArray);
-};
-
 export default function Crypto() {
-	const data = getData;
+	const [cryptoData, setCryptoData] = useState("");
+
+	useEffect(() => {
+		const getData = async () => {
+			let anArray = [];
+			const cryptoData = await getCryptoData();
+			renderData(cryptoData.slice(0, 5));
+		};
+		getData();
+	}, []);
+
+	const renderData = (cryptoArray) => {
+		let renderedCrypto = cryptoArray.map((data) => (
+			<div className="loadedCryptoData" key={data.name}>
+				<div className="cryptoData">
+					Name: {data.name} <br />
+					Price: {data.quote.USD.price}
+				</div>
+			</div>
+		));
+		setCryptoData(renderedCrypto);
+	};
 
 	return (
 		<div className="cryptoContainer">
 			<h1>Crypto</h1>
-			<button onClick={data}>Get Data</button>
+			{cryptoData}
+		</div>
+	);
+}
+
+
+	return (
+		<div className="cryptoContainer">
+			<h1>Crypto</h1>
+			{cryptoData}
 		</div>
 	);
 }
