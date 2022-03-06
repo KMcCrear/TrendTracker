b64f88e0acebc9a68469f7176119da152d597b40
TaylorJCassidy <taylorjcassidy1@gmail.com>
2022-03-06 21:47:40 +0000

    minor formatting

--- server/APIs/MarketCapAPI.js
+++ server/APIs/MarketCapAPI.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const router = express.Router();
-const {
-	MARKETCAPAPITOKEN,
-} = require("../config/tokens/MarketCapAPIToken.json");
+const { MARKETCAPAPITOKEN } = require("../config/tokens/MarketCapAPIToken.json");
 
 /**
  * @name SearchLatest
