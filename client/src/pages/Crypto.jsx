53c8808a5c56127e60bb659af79b1bb6289be0fc
KMcCrear <64017644+KMcCrear@users.noreply.github.com>
2022-03-13 15:46:17 +0000

    Crypto styling (#18)
    * Crypto data stored in table
    
    * Added rank to coins and moved table
    
    * Rounding to nearest decimal place

--- client/src/pages/Crypto.jsx
+++ client/src/pages/Crypto.jsx
@@ -8,26 +8,49 @@ export default function Crypto() {
 		const getData = async () => {
 			let anArray = [];
 			const cryptoData = await getCryptoData();
-			renderData(cryptoData.slice(0, 5));
+			renderData(cryptoData.slice(0, 12));
 		};
 		getData();
 	}, []);
 
 	const renderData = (cryptoArray) => {
+		let count = 0;
 		let renderedCrypto = cryptoArray.map((data) => (
-			<div className="loadedCryptoData" key={data.name}>
-				<div className="cryptoData">
-					Name: {data.name} <br />
-					Price: {data.quote.USD.price}
-				</div>
-			</div>
+			<table className="cryptoTable" key={data.name}>
+				<tbody className="loadedCryptoData">
+					<tr className="cryptoData">
+						<td>{(count += 1)}</td>
+						<td className="dataName">{data.name}</td>
+						<td className="dataPrice">{data.quote.USD.price}</td>
+						<td>{Math.round(data.quote.USD.percent_change_1h * 100) / 100}</td>
+						<td>{Math.round(data.quote.USD.percent_change_24h * 100) / 100}</td>
+						<td>{Math.round(data.quote.USD.percent_change_7d * 100) / 100}</td>
+						<td className="dataVolume">${data.quote.USD.volume_24h}</td>
+						<td className="dataMktCap">{data.quote.USD.market_cap}</td>
+					</tr>
+				</tbody>
+			</table>
 		));
 		setCryptoData(renderedCrypto);
 	};
 
 	return (
 		<div className="cryptoContainer">
-			<h1>Crypto</h1>
+			<h1 className="title">Crypto Prices by Market Cap</h1>
+			<table className="headerTable">
+				<thead className="columns">
+					<tr className="columnData">
+						<th>#</th>
+						<th>Coin</th>
+						<th>Price</th>
+						<th>1h</th>
+						<th>24h</th>
+						<th>7d</th>
+						<th>24h Volume</th>
+						<th>Mkt Cap</th>
+					</tr>
+				</thead>
+			</table>
 			{cryptoData}
 		</div>
 	);
