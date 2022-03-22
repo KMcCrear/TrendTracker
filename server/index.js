02fb670ca70f0774346b0e91074eca0b48b80d13
TaylorJCassidy <taylorjcassidy1@gmail.com>
2022-03-22 21:49:21 +0000

    moved imports to seperate routers

--- server/index.js
+++ server/index.js
@@ -2,19 +2,9 @@ const express = require("express");
 const cors = require("cors");
 
 const { HOST, PORT } = require("./config/host.json");
-
 const app = express();
 
 //middleware
-/*app.use(function (req, res, next) {
-	//res.header("Access-Control-Allow-Origin", "*");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Request-With, Content-Type, Accept"
-	);
-	next();
-});*/
-
 app.use(express.json());
 
 app.use(
@@ -25,20 +15,14 @@ app.use(
 	})
 );
 
-//API imports
-const twitterapi = require("./APIs/TwitterAPI");
-const polygonapi = require("./APIs/PolygonAPI");
-const marketcapapi = require("./APIs/MarketCapAPI");
-const openAiApi = require("./APIs/OpenAIAPI");
+//router imports
+const baseRouter = require("./Routers/BaseRouter");
+const authRouter = require("./Routers/AuthRouter")
 
-//API uses
-app.use("/twitter", twitterapi);
-app.use("/polygon", polygonapi);
-app.use("/marketcap", marketcapapi);
-app.use("/openai", openAiApi);
-
+//base api
+app.use('/',baseRouter)
 //authenticated API, must authenticate with login before accessing other routes
-app.use('/auth',require('./APIs/auth/authRouter'));
+app.use('/auth',authRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server Running on Port ${PORT}`);
