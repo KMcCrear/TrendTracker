28c61fd36eaef6f68a0cdff3ec164b9811aee9c6
TaylorJCassidy <taylorjcassidy1@gmail.com>
2022-03-23 14:27:26 +0000

    moved coingecko import to baserouter

--- server/Routers/BaseRouter.js
+++ server/Routers/BaseRouter.js
@@ -6,11 +6,13 @@ const twitterapi = require("../APIs/TwitterAPI");
 const polygonapi = require("../APIs/PolygonAPI");
 const marketcapapi = require("../APIs/MarketCapAPI");
 const openAiApi = require("../APIs/OpenAIAPI");
+const coingeckoapi = require("../APIs/CoinGeckoAPI")
 
 //API uses
 baseRouter.use("/twitter", twitterapi);
 baseRouter.use("/polygon", polygonapi);
 baseRouter.use("/marketcap", marketcapapi);
 baseRouter.use("/openai", openAiApi);
+baseRouter.use("/coingecko", coingeckoapi);
 
 module.exports = baseRouter;
