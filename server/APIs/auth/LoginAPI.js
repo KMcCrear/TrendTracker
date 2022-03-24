5d1d83927d8a945db701c1b7ba6c2de40e49f607
TaylorJCassidy <taylorjcassidy1@gmail.com>
2022-03-24 01:51:00 +0000

    improved response content

--- server/APIs/auth/LoginAPI.js
+++ server/APIs/auth/LoginAPI.js
@@ -1,5 +1,4 @@
 const auth = require("basic-auth");
-const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 
 /**
@@ -12,12 +11,12 @@ exports.login = function(db) {
 		if (req.session.user) {res.status(400).end('Already logged in'); return}
 		const credentials = auth(req);
 		if (!credentials) {
-			res.status(401).set('WWW-Authenticate', 'Basic realm="Access to user section", charset="UTF-8"').end();
+			res.status(401).set('WWW-Authenticate', 'Basic realm="Access to user section", charset="UTF-8"').end("Invalid or no credentials provided");
 		}
 		else {
 			db.query(`CALL getUser('${credentials.name}')`,(err,results,fields) => {
 				if (err) {res.status(500).end(); return}
-				if (results[0].length == 0) {res.status(401).end(); return}
+				if (results[0].length == 0) {res.status(401).end('Invalid uername or password'); return}
 
 				let user = results[0][0];
 
@@ -31,7 +30,7 @@ exports.login = function(db) {
 						res.status(200).end('Successfully logged in');
 					}
 					else {
-						res.status(401).end();
+						res.status(401).end('Invalid username or password');
 					}
 				});
 			});
