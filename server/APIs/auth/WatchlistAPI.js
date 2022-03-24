0507093fcec190d303f8594ca8c87a872332f7c7
TaylorJCassidy <taylorjcassidy1@gmail.com>
2022-03-24 22:46:55 +0000

    added initial add to watchlist functionality

--- server/APIs/auth/WatchlistAPI.js
+++ server/APIs/auth/WatchlistAPI.js
@@ -2,9 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/get',(req,res) => {
-	req.db.query(`CALL getUserwatchlist(${req.session.user.userID})`, (err,results,fields) => {
+	req.db.query('CALL getUserwatchlist(?)', req.session.user.userID, (err,results,fields) => {
 		if (err) {res.status(500).end(); return};
 		res.send(results[0]).end();
+	})
+})
+
+router.post('/add/:type/:id', (req,res) => {
+	const type = req.params.type;
+	const id = req.params.id;
+	req.db.query('CALL addToWatchlist(?,?,?)', req.session.user.userID, type, id,(err,results,fields) => {
+		if (err) {res.status(500).end(); return};
+
 	})
 })
 
