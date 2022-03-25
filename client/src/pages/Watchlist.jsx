3fec2b01a7c28544a55c82a88f857fd4eb4c98ea
TaylorJCassidy <taylorjcassidy1@gmail.com>
2022-03-25 21:08:01 +0000

    finished adding remove button

--- client/src/pages/Watchlist.jsx
+++ client/src/pages/Watchlist.jsx
@@ -1,29 +1,45 @@
-import React, { useEffect, useState } from "react";
+import axios from "axios";
+import endPoint from "../helpers/endPoint"
+import React, { useEffect, useRef, useState } from "react";
 import getWatchlist from "../helpers/getWatchlist";
 
 const Watchlist = () => {
-    const [watchlist, setWatchlist] = useState([]);
+    /*
+    * I know using a ref is an absolutly terrible way to fix this issue however I have yet to find a better way to do this
+    * The issue of stateWatchlist being empty within the remove function is due to a "stale closure", and the only way to
+    * fix it within a useEffect is to have it in the useEffect's dependecies, however this leads to the entire database
+    * retrieval process occuring again when a user clicks remove, as this changes the state of the watchlist which in turn
+    * overrides any actions that have took place anyway.
+    * https://dmitripavlutin.com/react-hooks-stale-closures/
+    */
+    const [stateWatchlist, setStateWatchlist] = useState([]);
+    const actualWatchlist = useRef([]);
     useEffect(() => {
+        function remove(id) {
+            axios({
+                method: 'DELETE',
+                url: `${endPoint()}/auth/watchlist/delete/${id}`,
+                withCredentials: true
+            }).then(() => {
+                actualWatchlist.current = actualWatchlist.current.filter(row => row.key != id);
+                setStateWatchlist(actualWatchlist.current);
+            }).catch(() => {
+                alert('Unable to remove');
+            })
+        }
+
         getWatchlist().then((data) => {
-            let renderedData = data.map((d) => (
+            actualWatchlist.current = data.map((d) => (
                 <tr key={d.listID}>
-                    <td>{d.what}</td>
                     <td>{d.identifier}</td>
+                    <td>{d.what}</td>
                     <td><a href={'/'}>View</a></td>
-                    <td><a onClick={remove.bind(null,d.listID)}>Remove</a></td>
+                    <td><a onClick={() => remove(d.listID)}>Remove</a></td>
                 </tr>
             ))
-            setWatchlist(renderedData);
-        }).catch((e) => {console.log(e)})   
-
-        function remove(id) {
-            //this doesnt work, watchlist is empty despite it being set previously
-            const newWatchlist = watchlist.filter(row => row.key != id);
-            setWatchlist(newWatchlist);
-        }   
+            setStateWatchlist(actualWatchlist.current);
+        }).catch((e) => {console.log(e)})
     },[])
-
-    
 
     return (
         <div>
@@ -36,7 +52,7 @@ const Watchlist = () => {
                             <th>Type</th>
                         </tr>
                     </thead>
-                    <tbody>{watchlist}</tbody>
+                    <tbody>{stateWatchlist}</tbody>
                 </table>
             </div>
         </div>
