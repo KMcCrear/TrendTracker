03f283efe06e540f572ea39c774b9e512c1d301c
KMcCrear <kmccrear1@gmail.com>
2022-03-26 16:47:21 +0000

    Routing to stock page from portfolio

--- client/src/pages/Stocks.jsx
+++ client/src/pages/Stocks.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import getFinanceData from "../helpers/getFinanceData";
+import CandleStick from "../components/CandleStick";
 
 export default function Stocks() {
-	return (
-		<div className="stocksContainer">
-			<h1>Stocks</h1>
-		</div>
-	);
+	const history = useLocation();
+	const params = history.pathname;
+	const query = params.slice(8);
+
+	useEffect(() => {
+		if (query) {
+			getFinanceData(query).then((data) => {
+				console.log(data);
+			});
+		}
+	});
+
+	const addToWatchList = () => {
+		//Method will call a helper to pass the userID type: stock and ID:ticker
+		console.log(query);
+	};
+
+	if (query) {
+		return (
+			<div className="stocksContainer">
+				<h1>Stocks</h1>
+				<CandleStick search={query} />
+				<button onClick={addToWatchList}>Add Ticker to Watch List</button>
+			</div>
+		);
+	} else {
+		return (
+			<div className="stocksContainer">
+				<h1>Stocks</h1>
+			</div>
+		);
+	}
 }
