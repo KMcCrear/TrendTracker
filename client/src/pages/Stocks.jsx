5d70cedd718633ce50bb87bcf9ead00e0bb7b54c
TaylorJCassidy <taylorjcassidy1@gmail.com>
2022-03-29 16:16:35 +0100

    removed button as now present in all candlesticks

--- client/src/pages/Stocks.jsx
+++ client/src/pages/Stocks.jsx
@@ -1,38 +1,19 @@
 import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import getFinanceData from "../helpers/getFinanceData";
 import CandleStick from "../components/CandleStick";
 import addToWatchList from "../helpers/addToWatchlist";
 
-export default function Stocks() {
+export default function Stocks(props) {
 	const history = useLocation();
-	const params = history.pathname;
-	const query = params.slice(8);
+	const {state} = props;
+	const {ticker} = useParams();
 
-	useEffect(() => {
-		if (query) {
-			getFinanceData(query).then((data) => {
-				console.log(data);
-			});
-		}
-	});
-
-	const addStockToWatchList = (e) => {
-		e.preventDefault();
-		const watchListID = query.toLowerCase().trim();
-		addToWatchList("stock", watchListID).then((response) => {
-			console.log(response);
-		});
-	};
-
-	if (query) {
+	if (ticker) {
 		return (
 			<div className="stocksContainer">
 				<h1>Stocks</h1>
-				<CandleStick search={query} />
-				<button onClick={(e) => addStockToWatchList(e)}>
-					Add Ticker to Watch List
-				</button>
+				<CandleStick search={ticker} state={state} what="stock"/>
 			</div>
 		);
 	} else {
