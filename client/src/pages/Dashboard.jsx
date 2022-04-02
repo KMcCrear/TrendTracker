55c4544d10192d41568e03715399cd5ba9d2e8b0
stefanbabukov <stefanbabukov98@gmail.com>
2022-04-02 21:04:16 +0200

    css changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,24 +1,48 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, {useState, useEffect} from "react";
 import CandleStick from "../components/CandleStick";
 import TweetInfo from "../components/TweetInfo";
 import NewsInfo from "../components/NewsInfo";
-
-import "antd/dist/antd.css";
+import '../css/Dashboard.css';
 import { Alert, Space } from "antd";
+import getTweets from '../helpers/getTweets';
 
 const Dashboard = (props) => {
 	const { search } = props;
 	const { state } = props;
+    const [tweets,setTweets] = useState([]);
+	
+    useEffect(()=>{
+        const getData = async()=>{
+            const data = await getTweets(search.toUpperCase());
+            setTweets(data);
+        }
+        getData();
+    },[search])
 
 	if (search) {
 		return (
 			<div className="dashboardContainer">
-				<h1>Dashboard</h1>
-				<Space direction="horizontal" style={{ width: "100%" }}>
-					<CandleStick search={search} state={state} what="stock"/>
-					<TweetInfo search={search} />
-				</Space>
-				<NewsInfo search={search} />
+					<div id='candleGraph'>
+						<CandleStick id='candleGraph' search={search} state={state} what="stock"/>
+					</div>
+					<div id='tweetInfo'>
+						<TweetInfo id='tweetInfo' tweets={tweets }search={search} />
+					</div>
+					<div id='sampleTweets' 
+						style={{display:'inline-block', 
+							'border-style': 'solid', 
+							'border-radius': '20px',
+							'margin-left': '5px',	
+							'background-color': '#6399B8',
+						}}>
+					<h3>Sample tweets about {search}</h3>
+
+                    	{tweets.data && tweets.data.slice(0,5).map(tweet=><p>{tweet.text}</p>)}
+                	</div>
+					<div id='newsInfo'>
+						<h2>News/Top articles</h2>
+						<NewsInfo tweets={tweets} search={search}/>
+					</div>
 			</div>
 		);
 	}
