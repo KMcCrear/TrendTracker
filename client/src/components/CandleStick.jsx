ca519afc660dea86906650769d6c7fbb8ae2fee4
TaylorJCassidy <taylorjcassidy1@gmail.com>
2022-04-06 17:11:48 +0100

    seperated charts into base and child components

--- client/src/components/CandleStick.jsx
+++ client/src/components/CandleStick.jsx
@@ -1,20 +1,14 @@
-import React, { useEffect, useState, useRef } from "react";
-import Chart from "react-apexcharts";
+import React from "react";
 import makeChartData from "../helpers/makeChartData";
 
-import addToWatchlist from "../helpers/addToWatchlist";
-import hasWatchlistItem from "../helpers/hasWatchlistItem";
-import deleteFromWatchlist from "../helpers/deleteFromWatchlist";
+import BaseChart from "./BaseChart";
 
 const CandleStick = (props)=>{
-	const propSearch = props.search?.toUpperCase();
+	const { search } = props;
 	const { state } = props;
 	const { what } = props;
 
-	const [search,setSearch] = useState(propSearch);
-	const [portfolioButton,setPortfolioButton] = useState();
-	const [data,setData] = useState([])
-	const options = useRef({
+	const options = {
 		chart: {
 			type: "candlestick",
 			height: 350,
@@ -43,66 +37,10 @@ const CandleStick = (props)=>{
 				fontFamily: undefined
 			}
 		}
-	})
-
-
-	const updateData = async()=>{
-		setData(await makeChartData(search))
-		if (state.loggedIn) {
-			const has = await hasWatchlistItem(search);
-			const onAdd = () => {
-				addToWatchlist(search,what)
-				.then(() => setPortfolioButton(remove))
-				.catch((err) => alert('unable'));
-			}
-			const onRemove = () => {
-				deleteFromWatchlist(search)
-				.then(() => setPortfolioButton(add))
-				.catch((err) => alert('unable'));
-			};
-			const add = <button onClick={onAdd}>Add to portfolio</button>;
-			const remove = <button onClick={onRemove}>Remove from portfolio</button>;
-			if (has) {
-				setPortfolioButton(remove);
-			}
-			else {
-				setPortfolioButton(add);
-			}
-		};
-	}
-
-	useEffect(()=>{
-		const fetchData = async()=>{
-			await updateData();	
-		}
-		fetchData();
-	},[search])
+	};
 
 	return (
-		<div id="chart">
-			<Chart
-				width={'250%'}
-				height={400}
-				options={options.current}
-				series={[{data}]}
-				type="candlestick"
-			/>
-			<div id="chartButtons">
-				<div>
-					<button onClick={updateData}>Refresh</button>
-					{portfolioButton}
-				</div>
-				<div>
-					<button onClick={() => setSearch(propSearch)}>1d</button>
-					<button onClick={() => setSearch(propSearch + '?range=week')}>7d</button>
-					<button onClick={() => setSearch(propSearch + '?range=month')}>30d</button>
-					<button onClick={() => setSearch(propSearch + '?range=quarter')}>90d</button>
-					<button onClick={() => setSearch(propSearch + '?range=year')}>1y</button>
-					<button onClick={() => setSearch(propSearch + '?range=2year')}>2y</button>
-				</div>
-			</div>
-			
-		</div>
+		<BaseChart state={state} what={what} options={options} getData={makeChartData} search={search}/>
 	);
 }
 export default CandleStick ;
--- client/src/components/TimeSeries.jsx
+++ client/src/components/TimeSeries.jsx
@@ -1,88 +1,82 @@
-import React, { useEffect, useState, useRef } from "react";
-import Chart from "react-apexcharts";
+import React from "react";
+import makeCoinData from "../helpers/makeCoinData";
 
-class TimeSeries extends React.Component {
-	constructor(props) {
-		super(props);
+import BaseChart from "./BaseChart";
 
-		this.state = {
-			series: [
-				{
-					name: "Price",
-					data: props.data,
-				},
-			],
-			options: {
-				chart: {
-					type: "area",
-					stacked: false,
-					height: 350,
-					zoom: {
-						type: "x",
-						enabled: true,
-						autoScaleYaxis: true,
-					},
-					toolbar: {
-						autoSelected: "zoom",
-					},
-				},
-				dataLabels: {
-					enabled: false,
-				},
-				markers: {
-					size: 0,
-				},
-				title: {
-					text: `${props.name} Stock Price Movement`,
-					align: "left",
-				},
-				fill: {
-					type: "gradient",
-					gradient: {
-						shadeIntensity: 1,
-						inverseColors: false,
-						opacityFrom: 0.5,
-						opacityTo: 0,
-						stops: [0, 90, 100],
-					},
-				},
-				yaxis: {
-					labels: {
-						formatter: function (val) {
-							return Math.round(val * 1) / 1;
-						},
-					},
-					title: {
-						text: "Price",
-					},
-				},
-				xaxis: {
-					type: "datetime",
-				},
-				tooltip: {
-					shared: false,
-					y: {
-						formatter: function (val) {
-							return Math.round(val * 100) / 100;
-						},
-					},
+const TimeSeries = (props) => {
+	const { search } = props;
+	const { state } = props;
+	const { what } = props;
+
+	const options = {
+		chart: {
+			type: "area",
+			stacked: false,
+			height: 350,
+			zoom: {
+				type: "x",
+				enabled: true,
+				autoScaleYaxis: true,
+			},
+			toolbar: {
+				autoSelected: "zoom",
+			},
+		},
+		dataLabels: {
+			enabled: false,
+		},
+		markers: {
+			size: 0,
+		},
+		fill: {
+			type: "gradient",
+			gradient: {
+				shadeIntensity: 1,
+				inverseColors: false,
+				opacityFrom: 0.5,
+				opacityTo: 0,
+				stops: [0, 90, 100],
+			},
+		},
+		yaxis: {
+			labels: {
+				formatter: function (val) {
+					return Math.round(val * 1) / 1;
 				},
 			},
-		};
-	}
+			title: {
+				text: "Price",
+			},
+		},
+		xaxis: {
+			type: "datetime",
+		},
+		tooltip: {
+			shared: false,
+			y: {
+				formatter: function (val) {
+					return Math.round(val * 100) / 100;
+				},
+			},
+		},
+		noData: {
+			text: "No data found",
+			align: 'center',
+			verticalAlign: 'middle',
+			offsetX: 0,
+			offsetY: 0,
+			style: {
+				color: undefined,
+				fontSize: '14px',
+				fontFamily: undefined
+			}
+		}
+	};
 
-	render() {
-		return (
-			<div id="chart">
-				<Chart
-					options={this.state.options}
-					series={this.state.series}
-					type="area"
-					height={350}
-				/>
-			</div>
-		);
-	}
+	return (
+		<BaseChart state={state} what={what} options={options} getData={makeCoinData} search={search}/>
+	)
+
 }
 
 export default TimeSeries;
