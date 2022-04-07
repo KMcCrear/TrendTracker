0a3d13c538ee64bedb4fe2953537c7a167e53a46
KMcCrear <64017644+KMcCrear@users.noreply.github.com>
2022-04-07 19:50:26 +0100

    fixes to login css (#45)

--- client/src/pages/Stocks.jsx
+++ client/src/pages/Stocks.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
+import getFinanceData from "../helpers/getFinanceData";
 import CandleStick from "../components/CandleStick";
 import getSingleTicker from "../helpers/getSingleTicker";
 import NewsInfo from "../components/NewsInfo";
@@ -10,10 +11,16 @@ export default function Stocks(props) {
 	const { ticker } = useParams();
 	const [stockData, setStockData] = useState("");
 	const [tweets, setTweets] = useState([]);
+	const [random, setRandom] = useState();
 
 	useEffect(() => {
 		const top5 = ["AAPL", "MSFT", "AMZN", "TSLA", "NVDA"];
+		let randomItem = top5[Math.floor(Math.random() * top5.length)];
+		setRandom(randomItem);
 		if (ticker) {
+			getFinanceData(ticker).then((data) => {
+				console.log(data);
+			});
 			const getTweetData = async () => {
 				const data = await getTweets(ticker.toUpperCase());
 				setTweets(data);
@@ -38,13 +45,18 @@ export default function Stocks(props) {
 								<td className="dataName">
 									<a href={`/stock/${response.symbol}`}>{response.symbol}</a>
 								</td>
-								<td className="dataPrice">{response.close}</td>
+								<td className="dataPrice">${response.close}</td>
 							</tr>
 						</tbody>
 					</table>
 				));
 				setStockData(renderData);
 			};
+			const getTweetData = async () => {
+				const data = await getTweets(random.toUpperCase());
+				setTweets(data);
+			};
+			getTweetData();
 		}
 	}, [ticker]);
 
@@ -53,8 +65,23 @@ export default function Stocks(props) {
 			<div className="stocksContainer">
 				<h1 className="heading">{ticker} Stock Price</h1>
 				<CandleStick search={ticker} state={state} what="stock" />
+				<div
+					id="sampleTweets"
+					style={{
+						display: "inline-block",
+						"border-style": "solid",
+						"border-radius": "20px",
+						"margin-left": "5px",
+						"background-color": "#6399B8",
+					}}
+				>
+					<h3>Sample tweets about {ticker}</h3>
+
+					{tweets.data &&
+						tweets.data.slice(0, 5).map((tweet) => <p>{tweet.text}</p>)}
+				</div>
 				<div id="newsInfo">
-					<NewsInfo search={ticker} />
+					<NewsInfo tweets={tweets} />
 				</div>
 			</div>
 		);
@@ -62,16 +89,39 @@ export default function Stocks(props) {
 		return (
 			<div className="stocksContainer">
 				<h1 className="heading">Trending Stocks</h1>
-				<table className="headerTable">
-					<thead className="columns">
-						<tr className="columnData">
-							<th>#</th>
-							<th>Stock</th>
-							<th>Price</th>
-						</tr>
-					</thead>
-				</table>
-				{stockData}
+				<div
+					id="sampleTweets"
+					style={{
+						display: "inline-block",
+						"align-content": "right",
+						float: "right",
+						"border-style": "solid",
+						"border-radius": "20px",
+						"margin-left": "5px",
+						"background-color": "#6399B8",
+						position: "static",
+					}}
+				>
+					<h3>Sample tweets about {random}</h3>
+
+					{tweets.data &&
+						tweets.data.slice(0, 5).map((tweet) => <p>{tweet.text}</p>)}
+				</div>
+				<div className="tableContainer">
+					<table className="headerTable">
+						<thead className="columns">
+							<tr className="columnData">
+								<th>#</th>
+								<th>Stock</th>
+								<th>Price</th>
+							</tr>
+						</thead>
+					</table>
+					{stockData}
+				</div>
+				<div id="newsInfo">
+					<NewsInfo tweets={random} />
+				</div>
 			</div>
 		);
 	}
