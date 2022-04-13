0ea51d4641b0a23a7ed9d1fa66d95ea796e21dc5
Taylor Cassidy <58339439+TaylorJCassidy@users.noreply.github.com>
2022-04-13 17:39:11 +0100

    Charts (#47)
    * removed switch button
    
    * coin gecko now supports range query
    
    * fixed width

--- server/APIs/CoinGeckoAPI.js
+++ server/APIs/CoinGeckoAPI.js
@@ -8,9 +8,37 @@ const router = express.Router();
  */
 router.get("/coins/market_chart/:search", (req, res) => {
 	const query = req.params.search;
+	let rangePast = 1
+
+	//query processing
+	//if object is empty
+	if (Object.keys(req.query).length != 0) {
+		//range
+		if (req.query.range != undefined) {
+			let range = req.query.range;
+			switch (range) {
+				case "week":
+					rangePast = 7;
+					break;
+				case "month":
+					rangePast = 30;
+					break;
+				case "quarter":
+					rangePast = 90;
+					break;
+				case "year":
+					rangePast = 360;
+					break;
+				case "2year":
+					rangePast = 720;
+					break;
+			}
+		}
+	}
+
 	axios({
 		method: "get",
-		url: `https://api.coingecko.com/api/v3/coins/${query}/market_chart?vs_currency=usd&days=31&interval=daily`,
+		url: `https://api.coingecko.com/api/v3/coins/${query}/market_chart?vs_currency=usd&days=${rangePast}`,
 	})
 		.then((response) => {
 			res.send(response.data);
