36b08d237af6f6af6a69eec60dc852f0d7234069
KMcCrear <64017644+KMcCrear@users.noreply.github.com>
2022-04-26 13:17:57 +0100

    Css match (#52)
    * css changes to match wireframes
    
    * navbar changes dashboard to home

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import SearchBar from "./SearchBar";
-import '../css/Navbar.css'
+import "../css/Navbar.css";
 
 function NavBar(props) {
 	const { state } = props;
@@ -9,16 +9,31 @@ function NavBar(props) {
 			<div className="Navbar">
 				<div className="leftSide">
 					<div className="links">
-						<a class='navBarLink' href="/">Dashboard</a>
-						<a class='navBarLink' href="/stock">Stocks</a>
-						<a class='navBarLink' href="/crypto">Crypto</a>
-						{state.loggedIn ? <a class='navBarLink' href="/logout">Logout</a>:<a class='navBarLink' href="/login">Login</a>}
-						<a class='navBarLink' href="/portfolio">Portfolio</a>
+						<a class="navBarLink" href="/">
+							Home
+						</a>
+						<a class="navBarLink" href="/stock">
+							Stocks
+						</a>
+						<a class="navBarLink" href="/crypto">
+							Crypto
+						</a>
+						{state.loggedIn ? (
+							<a class="navBarLink" href="/logout">
+								Logout
+							</a>
+						) : (
+							<a class="navBarLink" href="/login">
+								Login
+							</a>
+						)}
+						<a class="navBarLink" href="/portfolio">
+							Portfolio
+						</a>
 					</div>
-		
 				</div>
 				<div className="rightSide">
-					<SearchBar/>
+					<SearchBar />
 				</div>
 			</div>
 		</div>
